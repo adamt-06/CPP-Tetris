92489151a46549aab3101b2a21782fbb4e0eb676
Fatal-Dev <115042556+fatal-developer@users.noreply.github.com>
2023-12-28 20:08:01 +0000

    Modifed Background Colour

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 int main()
 {
+    Color darkBlue = {44, 44, 127, 255};
+
     InitWindow(300, 600, "C++ Tetris");
     SetTargetFPS(60);
 
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
-        
+        ClearBackground(darkBlue);
+
         EndDrawing();
     }
 
