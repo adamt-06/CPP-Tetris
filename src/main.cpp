b9c519223767c2c08d4b689e16eb5ed34800a143
Fatal-Dev <115042556+fatal-developer@users.noreply.github.com>
2023-12-28 21:43:21 +0000

    automatic block move down

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+
+    return false;
+}
+
 int main()
 {
     Color darkBlue = {44, 44, 127, 255};
@@ -13,6 +27,12 @@ int main()
     while(WindowShouldClose() == false)
     {
         game.HandleInput();
+
+        if(EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
+        
         BeginDrawing();
         ClearBackground(darkBlue);
 
